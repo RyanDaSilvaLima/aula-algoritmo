ecd4fa1dd1ce147a3fa65c56e21a8f7570256ee6
RYAN DA SILVA LIMA <ryansilva110612@gmail.com>
2025-09-17 20:26:59 -0300

    Create exercicio3.js

---
+++ lista-1/exercicio3.js
@@ -0,0 +1,8 @@
+let input = require('readline-sync');
+
+let n1= input.questionInt("digite um numero :");
+let n2=input.questionInt("digite outro numero :");
+
+let resp1= n1+n2;
+
+console.log("resposta:", resp1)
