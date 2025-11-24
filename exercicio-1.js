c85d46e6eb75dde4fa38c784d22754aa21787f87
RYAN DA SILVA LIMA <ryansilva110612@gmail.com>
2025-11-24 20:07:17 -0300

    Add files via upload

---
+++ exercicio-1.js
@@ -0,0 +1,11 @@
+const aluno = {
+    Nome: "Guilherme",
+    Idade: "30 anos",
+    Curso: "tecnologia"
+};
+
+function Apresentando (aluno){
+    return `O ${aluno.Nome} tem ${aluno.Idade} e esta cursando ${aluno.Curso}`;
+}
+
+console.log(Apresentando(aluno));
